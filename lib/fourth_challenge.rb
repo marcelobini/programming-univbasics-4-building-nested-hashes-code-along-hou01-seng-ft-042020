73722671249367cc430ea2dd584ea92e88b95adf
Marcelo Bini <marcelobini@gmail.com>
2020-03-26 21:54:40 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,13 +4,15 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [name: "Benvolio", age: "17", status: "worried"]
+      :hero_friends => [{name: "Benvolio", age: "17", status: "worried"}
+        {name: "Mercutio", age: "18", status: "hot-headed"}]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [name: "Mercutio", age: "18", status: "hot-headed"]
+      :heroine_friends => [{name: "Steven", age: "30", status: "confused"}
+        {name: "Nurse", age: "44", status: "worried"}]
    }
 }
 
