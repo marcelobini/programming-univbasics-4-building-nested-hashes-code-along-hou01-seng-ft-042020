2986da313f57670fa9bdbbfa5067a36d4ab65632
Marcelo Bini <marcelobini@gmail.com>
2020-03-26 21:36:24 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,15 +1,15 @@
-epic_tragedy = {
-   :montague => {
+def second_challenge
+  epic_tragedy = {
+    :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => []
-   },
-   :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-   }
-}
+      :hero_friends => [] 
+      
+      }, 
+    :capulet => {
+      
+      }}
+
+  
 end
